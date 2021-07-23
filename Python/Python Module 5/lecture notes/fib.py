ee82f544a3e7e0d7f1aaed88d141b680403f9d86
Yi J. Zhu <yi.zhu@berkeley.edu>
2021-07-22 20:47:21 -0700

    update fib

--- Python/Python Module 5/lecture notes/fib.py
+++ Python/Python Module 5/lecture notes/fib.py
@@ -1,19 +1,20 @@
 import time
 import ctypes
 
-### C implementation of fib ###
-# import fib function from fib.so DLL
+# C implementation of fib with Python wrapper
+# gcc -shared -o fib.so -fPIC fib.c
 _libc = ctypes.CDLL('fib.so')
-# python wrapper for C function
-def c_fib(n):
-	return _libc.fib(ctypes.c_int(n))
+c_fib = _libc.fib
+c_fib.restype = int
+c_fib.argtypes = [ctypes.c_int]
 
-### python implementation of fib ###
+# python implementation of fib
 def py_fib(n):
 	if n<2:
 		return n
 	return py_fib(n-1) + py_fib(n-2)
 
+#benchmark
 print('py_fib(36): ')
 start = time.time()
 print(f'\tresult: {py_fib(36)}')
